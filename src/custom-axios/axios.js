1434b2f261d3db41a6b7b09e986a6e71d2f8b85a
Nit4e <anitaterziska7@gmail.com>
2022-04-24 17:41:29 +0200

    URL changed

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:9090/api',
+  //baseURL: 'http://localhost:9090/api',
+  baseURL: 'https://emt-library.herokuapp.com/api',
   headers: {
     'Access-Control-Allow-Origin': '*'
   }
